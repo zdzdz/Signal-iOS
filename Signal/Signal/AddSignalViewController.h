c9858c0fcd9aeeeefccf3e3c35194a63e6379679
zdzdz <zdzdz.link@yahoo.com>
2016-02-01 23:32:22 +0200

    Added Views folder, AddSignalViewController and view in the storyboard

---
+++ Signal/Signal/AddSignalViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddSignalViewController.h
+//  Signal
+//
+//  Created by Sam Son on 2/1/16.
+//  Copyright © 2016 zdzdz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddSignalViewController : UIViewController
+
+@end
---
+++ Signal/Signal/AddSignalViewController.m
@@ -0,0 +1,27 @@
+//
+//  AddSignalViewController.m
+//  Signal
+//
+//  Created by Sam Son on 2/1/16.
+//  Copyright © 2016 zdzdz. All rights reserved.
+//
+
+#import "AddSignalViewController.h"
+
+@interface AddSignalViewController()
+
+@end
+
+@implementation AddSignalViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
